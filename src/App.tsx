deca51d1d13da66effda8359f2b3aac11dd206a1
Johans Neira <johansneirap@gmail.com>
2022-04-11 22:45:14 -0400

    style for main layout in dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,95 @@ import { useState } from 'react'
 import './App.css';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
-
+import { AppBar, Box, Divider, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { useTheme } from '@emotion/react';
+import MenuIcon from './icons/MenuIcon';
+const drawerWidth = 240;
 function App() {
-  const [drawerVisible, setDrawerVisible] = useState(false)
+  const [drawerVisible, setDrawerVisible] = useState(true);
+  const theme = useTheme();
 
   return (
     <Box className="App" sx={{ display: "flex"}}>
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Johans Neira Data Viz portfolio
-          </Typography>
-        </Toolbar>
-          <Button variant='contained' onClick={ ()=> setDrawerVisible(!drawerVisible)}>Drawer</Button>
+      <AppBar position="fixed" sx={{ 
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          backgroundColor: "white",
+          color: "black",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          gap: "1rem",
+          padding: "0.4rem 1rem",
+          alignItems: "center",
+          height: "65px",
+        }}>
+        <span><b> Johans Neira Data Viz Portfolio </b></span> 
+          <Button 
+            variant='outlined' 
+            onClick={ ()=> setDrawerVisible(!drawerVisible)}
+            style={{
+              padding: "0",
+              minWidth: "2rem",
+            }}>
+              <MenuIcon />
+          </Button>
       </AppBar>
       
     <Drawer
+      variant="persistent"
       anchor={'left'}
       open={ drawerVisible }
       onClose={ () => setDrawerVisible(false) }
-      sx={{top:"88px"}}
+      sx={{
+        width: 240,
+        flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 240,
+            boxSizing: 'border-box',
+          }
+      }}
     >
-      <Box
-        sx={{width:250}}>
-
-      </Box>
+      <Toolbar />
+      <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Dashboard', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? "1" : "2"}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? "1" : "2"}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
     </Drawer>
-      <main style={{backgroundColor: "#E3F2FD", height: "100vh"}}></main>
+    <div style={{
+      backgroundColor: "white", 
+      height: "calc(97vh - 62px)", 
+      width: "calc(100vw + 240px)", 
+      marginLeft: drawerVisible ? 0 : `-${drawerWidth}px`,
+      padding: "15px",
+      marginTop: "62px",
+      overflow: "hidden",
+      transition: "margin-left 0.2s",
+    }}>
+      <div style={{
+        backgroundColor: "#E3F2FD",
+        width: "100%",
+        height: "100%",
+      borderRadius: "25px",
+      }}></div>
+    </div>
     </Box>
   )
 }
