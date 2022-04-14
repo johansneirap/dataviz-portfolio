74db1cfa86cf2b6d03291240ba7668484a6b8468
Johans Neira <johansneirap@gmail.com>
2022-04-13 23:21:53 -0400

    add little design to boxes in top

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-const style = {
+interface StylesDictionary {
+    [key: string]: React.CSSProperties
+}
+const style: StylesDictionary = {
     mainDiv:{
         backgroundColor: "#E3F2FD",
         width: "100%",
@@ -8,7 +11,7 @@ const style = {
         padding: "20px",
         display: "grid",
         gridTemplateRows: "2fr 5fr",
-    },
+    } as React.CSSProperties,
     topBox:{
         border: "1px solid #ccc",
         display: "flex",
@@ -27,6 +30,8 @@ const style = {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        position: "relative",
+        overflow: "hidden",
     },
     card2: {
         backgroundColor: "#1E88E5", 
@@ -52,12 +57,34 @@ const style = {
         justifyContent: "center",
         alignItems: "center",
     },
-}
+    innerCircle: {
+        backgroundColor: "#512EA9",
+        height: "10rem",
+        width: "10rem",
+        borderRadius: "50%",
+        position: "absolute",
+        top: "-5rem",
+        right: "-1rem",
+        zIndex: "1",
+    },
+    innerCircle2: {
+        backgroundColor: "#4526A0",
+        height: "10rem",
+        width: "10rem",
+        borderRadius: "50%",
+        position: "absolute",
+        top: "-3rem",
+        right: "-5rem",
+        zIndex: "2",
+    },
+} 
 export const Dashboard = () => {
   return (
     <div style={{...style.mainDiv }}>
         <div style={{ ...style.topBox }}>
             <div style={{ ...style.card1 }}>
+                <div style={{ ...style.innerCircle }}></div>
+                <div style={{ ...style.innerCircle2 }}></div>
                 Dashboard resume card 1
             </div>
             <div style={{ ...style.card2 }}>
